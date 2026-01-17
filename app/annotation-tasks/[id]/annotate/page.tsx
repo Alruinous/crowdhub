23b797c499a638cfe1998c58e7e03da988e8ba28
罗睿安 <1969024607@qq.com>
2026-01-17 22:19:48 +0800

    next.config.mjs serverActions: {},

--- app/annotation-tasks/[id]/annotate/page.tsx
+++ app/annotation-tasks/[id]/annotate/page.tsx
@@ -758,7 +758,7 @@ export default function AnnotationPage({
           </h1>
         </div>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 max-w-[90rem]">
         <div className="lg:col-span-2">
           <Card>
             <CardHeader>
